4905fc6e929ba6821a5050d12d5713d89a934a29
Anthony Steele <anthony.steele@pockit.com>
2020-04-30 13:59:47 +0100

    Much expanded test cases also using a data driven "list of test cases" approach

--- tests/ReaderTest.cs
+++ tests/ReaderTest.cs
@@ -6,53 +6,16 @@ namespace NuKeeper.ProjectReader.Tests
 {
     public class ReaderTest
     {
-        [Test]
-        public void ReadEmpty()
+        [Test, TestCaseSource("ListTestCases")]
+        public void AllTestCases(string folderName, int expectedCount)
         {
             var testDataFolder = TestDataFolder();
 
-            var path = Path.Join(testDataFolder, "Empty");
-
+            var path = Path.Join(testDataFolder, folderName);
             var packages = Reader.FindAllNuGetPackages(path, new NullNuKeeperLogger());
 
             Assert.That(packages, Is.Not.Null);
-            Assert.That(packages, Is.Empty);
-        }
-
-        [Test]
-        public void ReadNetCoreProject()
-        {
-           var testDataFolder = TestDataFolder();
-
-            var path = Path.Join(testDataFolder, "NetCoreCsProj");
-            var packages = Reader.FindAllNuGetPackages(path, new NullNuKeeperLogger());
-
-            Assert.That(packages, Is.Not.Null);
-            Assert.That(packages, Is.Not.Empty);
-        }
-
-        [Test]
-        public void ReadFullFrameWorkProject()
-        {
-            var testDataFolder = TestDataFolder();
-
-            var path = Path.Join(testDataFolder, "FullFrameworkCsProj");
-            var packages = Reader.FindAllNuGetPackages(path, new NullNuKeeperLogger());
-
-            Assert.That(packages, Is.Not.Null);
-            Assert.That(packages, Is.Not.Empty);
-        }
-
-        [Test]
-        public void ReadDirBuildPropsProject()
-        {
-            var testDataFolder = TestDataFolder();
-
-            var path = Path.Join(testDataFolder, "DirBuildProps");
-            var packages = Reader.FindAllNuGetPackages(path, new NullNuKeeperLogger());
-
-            Assert.That(packages, Is.Not.Null);
-            Assert.That(packages, Is.Not.Empty);
+            Assert.That(packages.Count, Is.EqualTo(expectedCount));
         }
 
         private static string TestDataFolder()
@@ -62,5 +25,20 @@ namespace NuKeeper.ProjectReader.Tests
             var sep = Path.DirectorySeparatorChar;
             return rootInfo.FullName + sep + "TestData";
         }
+
+        // picked up as an "unused field", but it is used via TestCaseSource attribute above
+#pragma warning disable CA1823
+        private static readonly object[] ListTestCases =
+        {
+            new object[] { "Empty", 0 },
+            new object[] { "FullFrameworkCsProj", 1 },
+            new object[] { "FullFrameworkVbProj", 1 },
+            new object[] { "NetCoreCsProj", 1 },
+            new object[] { "NetCoreVbProj", 1 },
+            new object[] { "NetCoreFsProj", 1 },
+            new object[] { "TwoNetCoreCsProj", 4 },
+            new object[] { "DirBuildProps", 1 },
+            new object[] { "DirBuildPropsMixed", 2 },
+        };
     }
 }
