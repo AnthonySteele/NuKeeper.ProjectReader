176fc940610378f9032d9edf76b459aae3e5e76f
Anthony Steele <anthony.steele@pockit.com>
2020-05-01 13:21:47 +0100

    Coverage report -> html with "reportGenerator"

--- cover.bat
+++ cover.bat
@@ -1,8 +1,13 @@
 echo off
 REM generate a coverage report locally
-REM if this does not work, first do a
+REM output in .\coveragereport
+REM if this does not work, first install the tools:
+
 REM dotnet tool install --global coverlet.console
+REM dotnet tool install --global dotnet-reportgenerator-globaltool
 
 dotnet build
 
-coverlet .\tests\bin\Debug\netcoreapp3.1\NuKeeper.ProjectReader.Tests.dll --target "dotnet" --targetargs "test --no-build"
+coverlet .\tests\bin\Debug\netcoreapp3.1\NuKeeper.ProjectReader.Tests.dll --target "dotnet" --targetargs "test --no-build" --format opencover
+
+reportgenerator -reports:.\coverage.opencover.xml -targetdir:.\coveragereport
